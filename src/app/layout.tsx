2317a61569531005c49e0b76f12705bd19024353
taykim01 <taykim01@gmail.com>
2024-10-28 13:58:39 +0900

    fix: 모바일 화면 기본으로 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,16 @@ export default function RootLayout({
   return (
     <html lang="kr">
       <RecoilRootWrapper>
-        <body className={pretendard.variable}>{children}</body>
+        <body
+          className={
+            pretendard.variable +
+            " w-screen h-screen flex items-center justify-center bg-gray-100"
+          }
+        >
+          <div className="max-w-[393px] w-full max-h-[750] h-full bg-white rounded-3xl">
+            {children}
+          </div>
+        </body>
       </RecoilRootWrapper>
     </html>
   );
